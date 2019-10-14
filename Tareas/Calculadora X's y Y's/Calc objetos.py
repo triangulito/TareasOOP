e784b9fde733a5e10325262102862968534bab4b
Daniel <acruz@cetys.edu.mx>
2019-10-14 16:16:02 -0700

    Actualizacion de tarea Calculadora

--- Tareas/Calculadora X's y Y's/Calc objetos.py
+++ Tareas/Calculadora X's y Y's/Calc objetos.py
@@ -2,7 +2,11 @@ def valid(entrada):
     a='xy'
     for i in entrada:
         if i not in a:
+            print('El dato ingresado cuenta con un caracter invalido')
             return False
+    if 'yyyy' not in entrada:
+        print("El dato ingresado no cuenta con un signo '='")
+        return False
     return True
 
 def calc(entrada):
@@ -17,7 +21,8 @@ def calc(entrada):
         elif x=='0':
             continue
         t+=x
-    print(eval(t))
+    print(f'Proceso a hacer: {t}')
+    print(f'Resultado: {eval(t)}')
     
 dato=input("\t\tCalculadora de X's y Y's\nIngrese su dato: ").lower()
 
